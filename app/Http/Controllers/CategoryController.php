67047179aff2e3de4eb2485f7c05348fdfce25ef
decio <decio.crf@gmail.com>
2018-01-22 12:06:06 -0300

    Inittial routes created, controllers revised.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
 
 class CategoryController extends Controller
 {
@@ -14,6 +15,7 @@ class CategoryController extends Controller
     public function index()
     {
         //
+       return $categories = Category::all();
     }
 
     /**
--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Company;
 
 class CompanyController extends Controller
 {
@@ -14,6 +15,7 @@ class CompanyController extends Controller
     public function index()
     {
         //
+        return $companies = Company::all();
     }
 
     /**
